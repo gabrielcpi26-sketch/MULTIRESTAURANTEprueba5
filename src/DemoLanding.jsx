d28d347ccce1fcd01c022814d1dbd4d984c2a2f7
Gabriel <gabriel.cpi26@gmail.com>
2025-11-27 18:29:18 -0600

    create vercel page

--- src/DemoLanding.jsx
+++ src/DemoLanding.jsx
@@ -1,7 +1,5 @@
 // src/DemoLanding.jsx
 import React, { useState } from "react";
-
-import DemoLanding from "./DemoLanding";
 const DemoLanding = () => {
   const [activeDemo, setActiveDemo] = useState("restaurante");
 
